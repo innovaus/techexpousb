9af51a965536203ad0c62cfed0c4f22ec12fe31a
Meganathan, Sathiskumar <sathiskumar.meganathan@usbank.com>
2017-04-06 13:56:46 -0700

    testing sample FB templates

--- routes/routes.js
+++ routes/routes.js
@@ -213,13 +213,6 @@ var handleWelcomeIntent = function(req, res) {
             "title":"Welcome to Peter\'s Hats",
             "image_url":"https://petersfancybrownhats.com/company_image.png",
             "subtitle":"We\'ve got the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-              "messenger_extensions": true,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-            },
             "buttons":[
               {
                 "type":"web_url",
